66f92b7acfc7db933d7a889d13cef3852dd1b32d
古娜 <shiti08@126.com>
2017-06-02 19:59:32 +0800

    Remove Springboot support

--- src/main/java/com/qingzhenyun/common/QingZhenYunCommonApplication.java
+++ src/main/java/com/qingzhenyun/common/QingZhenYunCommonApplication.java
@@ -1,12 +1,9 @@
 package com.qingzhenyun.common;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class QingZhenYunCommonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QingZhenYunCommonApplication.class, args);
+
 	}
 }
