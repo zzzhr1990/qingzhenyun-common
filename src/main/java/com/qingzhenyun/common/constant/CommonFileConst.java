713f7dd6eb74177eba7e37b79c0fbac7033c1f98
古娜 <shiti08@126.com>
2017-06-03 00:06:21 +0800

    add some constants

---
+++ src/main/java/com/qingzhenyun/common/constant/CommonFileConst.java
@@ -0,0 +1,10 @@
+package com.qingzhenyun.common.constant;
+
+/**
+ * Created by guna on 2017/5/13.
+ */
+public class CommonFileConst {
+    public static final int FILE_STATUS_NORMAL = 0;
+    public static final int DIRECTORY = 1;
+    public static final int FILE = 0;
+}
--- src/main/java/com/qingzhenyun/common/constant/MqConst.java
+++ src/main/java/com/qingzhenyun/common/constant/MqConst.java
@@ -10,5 +10,7 @@ public class MqConst {
     public static final String OFFLINE_ADD_TORRENT_TASK_KEY = "torrent-task.add";
     public static final String OFFLINE_TORRENT_PRE_PARSED_KEY = "torrent-task.pre_parse";
     public static final String OFFLINE_TORRENT_FILE_UPDATE = "torrent-task.file_update";
+    public static final String OFFLINE_UPLOAD_CHECK_ROUTING_KEY = "offline.upload.check";
+
 }
 
