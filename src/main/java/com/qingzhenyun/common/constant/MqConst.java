82351162b1188c07025b5a1d22c7d5a3185d0b1a
古娜 <shiti08@126.com>
2017-06-03 00:02:09 +0800

    add some constants

---
+++ src/main/java/com/qingzhenyun/common/constant/MqConst.java
@@ -0,0 +1,14 @@
+package com.qingzhenyun.common.constant;
+
+/**
+ * MQC
+ * Created by guna on 2017/5/13.
+ */
+public class MqConst {
+    public static final String OFFLINE_EXCHANGE = "offline-exchange";
+    public static final String OFFLINE_ADD_ROUTING_KEY = "offline.add";
+    public static final String OFFLINE_ADD_TORRENT_TASK_KEY = "torrent-task.add";
+    public static final String OFFLINE_TORRENT_PRE_PARSED_KEY = "torrent-task.pre_parse";
+    public static final String OFFLINE_TORRENT_FILE_UPDATE = "torrent-task.file_update";
+}
+
---
+++ src/main/java/com/qingzhenyun/common/constant/TorrentConst.java
@@ -0,0 +1,13 @@
+package com.qingzhenyun.common.constant;
+
+/**
+ * d
+ * Created by guna on 2017/5/14.
+ */
+public class TorrentConst {
+    public static final int PRETENDING_PROCESS_SUCCESS = 1;
+    public static final int PRE_PROCESS_SUCCESS = 10;
+    public static final int STATUS_START_DOWNLOAD = 5;
+    public static final int TYPE_TORRENT = 1;
+    public static final int TYPE_MAGNET = 2;
+}
