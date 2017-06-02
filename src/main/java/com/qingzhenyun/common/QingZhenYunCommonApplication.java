d4f55848ae541d00fa46162e34d19c398c97c96c
古娜 <shiti08@126.com>
2017-06-02 19:23:47 +0800

    Initial commit

---
+++ src/main/java/com/qingzhenyun/common/QingZhenYunCommonApplication.java
@@ -0,0 +1,12 @@
+package com.qingzhenyun.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingZhenYunCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QingZhenYunCommonApplication.class, args);
+	}
+}
---
+++ src/main/java/com/qingzhenyun/common/torrent/InvalidBEncodingException.java
@@ -0,0 +1,17 @@
+package com.qingzhenyun.common.torrent;
+
+import java.io.IOException;
+
+/**
+ * Exception thrown when a B-encoded stream cannot be decoded.
+ *
+ * @author mpetazzoni
+ */
+public class InvalidBEncodingException extends IOException {
+
+    public static final long serialVersionUID = -1;
+
+    public InvalidBEncodingException(String message) {
+        super(message);
+    }
+}
