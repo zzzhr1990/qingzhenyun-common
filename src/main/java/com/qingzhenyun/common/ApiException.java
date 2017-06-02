89615decddfd1c2e592547bf0040902d93ffa561
古娜 <shiti08@126.com>
2017-06-02 23:16:28 +0800

    Add magnet support

---
+++ src/main/java/com/qingzhenyun/common/ApiException.java
@@ -0,0 +1,55 @@
+package com.qingzhenyun.common;
+
+import com.qingzhenyun.common.entity.ApiResults;
+
+public class ApiException extends RuntimeException {
+    private final int code;
+
+    public ApiException() {
+        super("INTERNAL_SERVER_ERROR");
+        this.code = 500;
+    }
+
+    public ApiException(int code) {
+        this.code = code;
+    }
+
+    public ApiException(String message) {
+        super(message);
+        this.code = 500;
+    }
+
+    public ApiException(ApiResults status, String message) {
+        super(message);
+        this.code = status.value();
+    }
+
+    public ApiException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public ApiException(int code, String message, Throwable cause) {
+        super(message, cause);
+        this.code = code;
+    }
+
+    public ApiException(ApiResults status, String message, Throwable cause) {
+        super(message, cause);
+        this.code = status.value();
+    }
+
+    public ApiException(int code, Throwable cause) {
+        super(cause);
+        this.code = code;
+    }
+
+    public ApiException(int code, String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.code = code;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+}
