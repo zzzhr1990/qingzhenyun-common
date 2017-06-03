1ac118a2a97ac508972372413856bd17202deeaf
古娜 <shiti08@126.com>
2017-06-04 00:34:39 +0800

    add okhttp

---
+++ src/main/java/com/qingzhenyun/common/util/HttpUtil.java
@@ -0,0 +1,48 @@
+package com.qingzhenyun.common.util;
+
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by guna on 2017/6/4.
+ */
+@Slf4j
+public class HttpUtil {
+    //
+    private static final OkHttpClient client = new OkHttpClient();
+
+    public InputStream getRemoteByte(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) {
+            throw new IOException("Unexpected code " + response);
+        }
+        ResponseBody body = response.body();
+        if (body == null) {
+            throw new IOException("Response empty code " + response);
+        }
+        return body.byteStream();
+    }
+
+    public InputStream getRemoteByte(String url, Integer tryTime) throws IOException {
+        if (tryTime < 0) {
+            throw new IOException("Request url " + url + "failed.");
+        }
+        tryTime--;
+        try {
+            return getRemoteByte(url);
+        } catch (IOException ex) {
+            log.warn("Request {} failed, {} left.", url, tryTime, ex);
+            return getRemoteByte(url, tryTime);
+        }
+    }
+
+}
